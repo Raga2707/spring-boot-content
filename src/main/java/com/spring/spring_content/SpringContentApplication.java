19a086e9c3e28a60620652696b3ca48f605798c3
Raga2707 <ragamalya295@gmail.com>
2026-07-27 23:52:54 +0530

    Created Spring Boot Application

---
+++ src/main/java/com/spring/spring_content/SpringContentApplication.java
@@ -0,0 +1,13 @@
+package com.spring.spring_content;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContentApplication.class, args);
+	}
+
+}
